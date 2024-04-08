de38f5decb54d253aba898a669e275abd35c6766
TehDomic <47988050+TehDomic@users.noreply.github.com>
2024-04-08 17:34:02 +0200

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+# Fish-like setup.
+# Tutorial: https://khushwant.hashnode.dev/powershell-autosuggestions-like-fishzsh-autosuggestions
+
+Import-Module PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#875f5f'}
+
+Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+
+# WinGet Tab Completion Feature
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
